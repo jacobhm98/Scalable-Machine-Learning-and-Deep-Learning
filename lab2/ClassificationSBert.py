88900ab66aee56a9a101875a8e3c325047c0fe77
Jacob Hedén Malm <jmalm@invidi.com>
2021-12-19 16:00:56 +0100

    began work pon classification objective

---
+++ lab2/ClassificationSBert.py
@@ -0,0 +1,42 @@
+import torch
+from transformers import BertTokenizer, BertModel
+
+
+class SBert(torch.nn.Module):
+
+    def __init__(self):
+        super(SBert, self).__init__()
+        self.regression = False
+        self.bert_layer = BertModel.from_pretrained('bert-base-uncased')
+        self.cosine_similarity = torch.nn.CosineSimilarity(dim=1)
+        self.softmax = torch.nn.Softmax()
+        self.fc = torch.nn.Linear(in_features=(768 * 3), out_features=3)
+
+    def forward(self, sentences):
+        sent1 = sentences[0]
+        sent2 = sentences[1]
+        left_bert_output = torch.mean((self.bert_layer(input_ids=sent1["input_ids"].squeeze(0))).last_hidden_state,
+                                      dim=1)
+        right_bert_output = torch.mean((self.bert_layer(input_ids=sent2["input_ids"].squeeze(0))).last_hidden_state,
+                                       dim=1)
+        if not self.regression:
+            x = self.concatenate_tensors(left_bert_output, right_bert_output)
+            x = self.softmax(x)
+        else:
+            x = self.cosine_similarity(left_bert_output, right_bert_output)
+        return x
+
+    def freeze_bert_params(self):
+        for param in self.bert_layer.parameters():
+            param.requires_grad = False
+
+    def unfreeze_bert_params(self):
+        for param in self.bert_layer.parameters():
+            param.requires_grad = True
+
+    def regression_objective(self, is_regression_objective):
+        self.regression = is_regression_objective
+
+    def concatenate_tensors(self, u, v):
+        diff = torch.sub(u, v)
+        return torch.cat((u, v, diff))
