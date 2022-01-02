abd454fce726433b7a580388c8e36d00fc108695
Jacob Hedén Malm <jacmalm@kth.se>
2022-01-02 18:16:48 +0700

    work

--- lab2/main.py
+++ lab2/main.py
@@ -8,6 +8,7 @@ from scipy.stats import spearmanr
 
 
 def main():
+    #model = train_regression_model(model_file="./regression_s_bert_2.model")
     model = train_classification_model()
 
 def train_classification_model(model_file=None):
@@ -17,11 +18,12 @@ def train_classification_model(model_file=None):
         model = torch.load(model_file)
     dataset = SNLIDataset()
     dataloader = DataLoader(dataset=dataset)
+    model.train()
 
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
     running_loss = 0
-    for epoch in range(3):
+    for epoch in range(1):
         for i, data_point in enumerate(dataloader):
             features, label = data_point
             optimizer.zero_grad()
@@ -34,12 +36,14 @@ def train_classification_model(model_file=None):
             if i % 1000 == 999:
                 print(i, running_loss)
                 running_loss = 0
+            if i % 10000 == 9999:
+                model.regression_objective(is_regression_objective=True)
+                evaluate_model(model)
+                model.regression_objective(is_regression_objective=False)
+                model.train()
+                torch.save(model, "classification_s_bert_" + str(i) + ".model")
             loss.backward()
             optimizer.step()
-        model.regression_objective(is_regression_objective=True)
-        evaluate_model(model)
-        model.regression_objective(is_regression_objective=False)
-        torch.save(model, "classification_s_bert_" + str(epoch) +  ".model")
     return model
 
 
@@ -48,14 +52,15 @@ def train_regression_model(model_file=None):
         model = RegressionSBert()
     else:
         model = torch.load(model_file)
+        model.regression_objective(is_regression_objective=True)
     train_set = STSDataset.StsDataset('./stsbenchmark/sts-train.csv')
     data_loader = DataLoader(dataset=train_set, shuffle=True)
 
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
-    model.train()
+    optimizer = torch.optim.Adam(model.parameters())
     running_loss = 0
-    for epoch in range(1):
+    for epoch in range(3):
+        model.train()
         for i, data_point in enumerate(data_loader):
             features, target = data_point
             optimizer.zero_grad()
@@ -67,13 +72,15 @@ def train_regression_model(model_file=None):
                 running_loss = 0
             loss.backward()
             optimizer.step()
-        torch.save(model, "regression_s_bert.model")
+        evaluate_model(model)
+        torch.save(model, "regression_s_bert_" + str(3 + epoch) + ".model")
     return model
 
 def evaluate_model(model):
     test_set = STSDataset.StsDataset('./stsbenchmark/sts-test.csv')
     test_data_loader = DataLoader(dataset=test_set)
     model.eval()
+    model.requires_grad_(False)
     predicted = []
     real_values = []
     for data_point in test_data_loader:
@@ -82,6 +89,7 @@ def evaluate_model(model):
         predicted.append(outputs.item())
         real_values.append(target.item())
     spearman_coefficient, p_val = spearmanr(predicted, real_values)
+    model.requires_grad_(True)
     print("Observed correlation", spearman_coefficient)
 
 
